90b93faf1d2ba3d0096df562b86302ae7bf9bb96
Shawn Lin Jingjue <e0003823@u.nus.edu>
2017-03-24 15:01:11 +0800

    Abstract ChatMessage so that SuruChatMessage and UserChatMessage can share the same properties.

--- src/main/java/seedu/task/ui/chat/SuruChatMessage.java
+++ src/main/java/seedu/task/ui/chat/SuruChatMessage.java
@@ -1,23 +1,12 @@
 package seedu.task.ui.chat;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
 import seedu.task.model.chat.Chat;
-import seedu.task.ui.UiPart;
 
-public class SuruChatMessage extends UiPart<Region> {
+public class SuruChatMessage extends ChatMessage {
     protected static final String FXML = "SuruChatMessage.fxml";
 
-    @FXML
-    private StackPane chatMessagePane;
-    @FXML
-    protected Label message;
-
     public SuruChatMessage(Chat chat) {
-        super(FXML);
-        message.setText(chat.getMessage());
+        super(FXML, chat);
     }
 
 }
