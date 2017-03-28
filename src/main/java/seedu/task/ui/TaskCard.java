ccaf91f31eda797bc3275bd9e07adf09167051dd
Shawn Lin Jingjue <e0003823@u.nus.edu>
2017-03-28 17:31:50 +0800

    Ui for checked and unchecked done

--- src/main/java/seedu/task/ui/TaskCard.java
+++ src/main/java/seedu/task/ui/TaskCard.java
@@ -2,6 +2,7 @@ package seedu.task.ui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
@@ -20,23 +21,43 @@ public class TaskCard extends UiPart<Region> {
     @FXML
     private Label startTime;
     @FXML
-    private Label completionStatus;
-    @FXML
     private Label endTime;
     @FXML
     private FlowPane tags;
+    @FXML
+    private ImageView imgChecked;
+    @FXML
+    private ImageView imgUnchecked;
 
     public TaskCard(ReadOnlyTask task, int displayedIndex) {
         super(FXML);
         name.setText(task.getName().fullName);
         id.setText(displayedIndex + ". ");
         startTime.setText(task.getStartTime().value);
-        completionStatus.setText(task.getCompletionStatus().toString());
+        setCheckboxStatus(task.getCompletionStatus().getStatus());
         endTime.setText(task.getEndTime().value);
         initTags(task);
     }
 
     private void initTags(ReadOnlyTask task) {
         task.getTags().forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+    }
+
+    private void setCheckboxStatus(boolean completionStatus){
+        if(completionStatus){
+            showChecked();
+        }else{
+            showUnchecked();
+        }
+    }
+
+    private void showUnchecked() {
+        imgChecked.setVisible(false);
+        imgUnchecked.setVisible(true);
+    }
+
+    private void showChecked() {
+        imgChecked.setVisible(true);
+        imgUnchecked.setVisible(false);
     }
 }
