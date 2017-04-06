5de5c6d79283858e747566151d72bd3ba9ce9cf0
Shawn Lin Jingjue <e0003823@u.nus.edu>
2017-04-06 17:27:25 +0800

    add background, change colors of message boxes

--- src/test/java/guitests/HelpWindowTest.java
+++ src/test/java/guitests/HelpWindowTest.java
@@ -11,12 +11,6 @@ public class HelpWindowTest extends TaskManagerGuiTest {
 
     @Test
     public void openHelpWindow() {
-        //use accelerator
-        commandBox.clickOnTextField();
-        assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
-//
-//        resultDisplay.clickOnTextArea();
-//        assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
 
         taskListPanel.clickOnListView();
         assertHelpWindowOpen(mainMenu.openHelpWindowUsingAccelerator());
