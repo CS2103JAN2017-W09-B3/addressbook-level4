c50495898d32830407cf5c3d2aa390cecd1c3d8a
amon <amon@nandynarwhals.org>
2017-04-07 11:13:46 +0800

    NattyDateUtil.parse() now sets a time explicitly for inferred timings when parsing

--- src/main/java/seedu/task/commons/util/NattyDateUtil.java
+++ src/main/java/seedu/task/commons/util/NattyDateUtil.java
@@ -1,10 +1,15 @@
 package seedu.task.commons.util;
 
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Logger;
 
 import com.joestelmach.natty.DateGroup;
 import com.joestelmach.natty.Parser;
+
+import seedu.task.commons.core.LogsCenter;
+import seedu.task.logic.parser.AddCommandParser;
 
 //@@author A0146789H
 /**
@@ -13,6 +18,9 @@ import com.joestelmach.natty.Parser;
  * This class provides utility functions to flexibly parse Dates from a string.
  */
 public class NattyDateUtil {
+
+    private static final Logger logger = LogsCenter.getLogger(AddCommandParser.class);
+    private static final String logPrefix = "[AddCommandParser]";
 
     /**
      * Parse a string into a single date object using Natty.
@@ -24,12 +32,33 @@ public class NattyDateUtil {
         Parser parser = new Parser();
         List<DateGroup> groups = parser.parse(dataString);
 
+        logger.info(String.format("%s parsing date string '%s'", logPrefix, dataString));
+
         // Check if there are any results. If not, return null.
         if (groups.isEmpty() || groups.get(0).getDates().isEmpty()) {
+            logger.info(String.format("%s failed to parse date string", logPrefix));
             return null;
         }
 
-        return groups.get(0).getDates().get(0);
+        DateGroup dg = groups.get(0);
+        Date result = dg.getDates().get(0);
+
+        // Log information about the date parsed
+        logger.info(String.format("%s date: %s, isDateInferred(): %s, isTimeInferred(): %s",
+                    logPrefix, result.toString(), dg.isDateInferred(), dg.isTimeInferred()));
+
+        // If the time was inferred, set the time explicitly to (high) noon.
+        Calendar modified = Calendar.getInstance();
+        modified.setTime(result);
+        modified.set(Calendar.HOUR, 12);
+        modified.set(Calendar.MINUTE, 0);
+        modified.set(Calendar.SECOND, 0);
+        modified.set(Calendar.MILLISECOND, 0);
+        Date newResult = modified.getTime();
+
+        logger.info(String.format("%s newResult: %s", logPrefix, newResult.toString()));
+
+        return newResult;
     }
 
 }
